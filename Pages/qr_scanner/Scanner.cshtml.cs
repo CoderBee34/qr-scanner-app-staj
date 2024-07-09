93bccae7b1cd86250bb3783ebf06a930dc9ec171
ali ilan <ali.ilan@ozu.edu.tr>
2024-07-09 18:33:20 +0300

    screenshot alıyor fakat birden fazla defa alıyor bunu çözmeye çalışıyorum.

--- Pages/qr_scanner/Scanner.cshtml.cs
+++ Pages/qr_scanner/Scanner.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 namespace qr_scanner_app_staj.Pages.qr_scanner
 {
     public class ScannerModel() : PageModel
@@ -9,6 +11,25 @@ namespace qr_scanner_app_staj.Pages.qr_scanner
         public async Task<IActionResult> OnPost()
         {
             Console.WriteLine(QrCode);
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                driver.Navigate().GoToUrl(QrCode);
+                Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+
+                string filePath = Path.Combine(Environment.CurrentDirectory, "screenshot.png");
+                screenshot.SaveAsFile(filePath);
+
+                Console.WriteLine($"Screenshot saved to: {filePath}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+            finally
+            {
+                driver.Quit();
+            }
             return RedirectToPage("Receipts");
         }
     }
